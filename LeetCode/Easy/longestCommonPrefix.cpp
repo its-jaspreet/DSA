db88c03e09e5ff82dead83e9aea9d0a9117a045e
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-16 21:42:36 +0530

    created and added template code to longestCommonPrefix.cpp

---
+++ LeetCode/Easy/longestCommonPrefix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       
+    }
+};
+
+int main() {
+    return 0;
+}
