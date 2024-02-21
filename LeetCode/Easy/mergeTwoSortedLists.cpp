283387d0991dbad844dd4b7973117530bf223280
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-21 23:19:10 +0530

    code so far

---
+++ LeetCode/Easy/mergeTwoSortedLists.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* p, q;
+        p = list1;
+
+    }
+};
