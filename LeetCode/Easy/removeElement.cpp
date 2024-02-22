a67eaa447586c29fdd566495a2cb667d1be29bd8
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-22 16:45:15 +0530

    added new test cases to removeElement.cpp

--- LeetCode/Easy/removeElement.cpp
+++ LeetCode/Easy/removeElement.cpp
@@ -6,22 +6,31 @@ using namespace std;
 class Solution {
 public:
     vector<int> removeElement(vector<int>& nums, int val) {
-        int i = 0, j = nums.size() - 1, ctr = 0;
-        while (i <= j) {
-            if (nums[j] == val) {
-                j -= 1;
-            }
-            if (nums[i] != val) {
-                i += 1;
+        if (nums.size() == 0){
+            return nums;
+        }
+        if (nums.size() == 1) {
+            if (nums[0] == val) {
+                nums.pop_back();
+                return nums;
             } else {
-                int temp;
-                temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                ctr++;
+                return nums;
             }
         }
-        for (int k = 0; k <= ctr; k++) {
+        int i = 0, j = nums.size() - 1, ctr = 0;
+        while (i <= j) {
+            if (nums[i] == val) {
+                if (nums[j] != val) {
+                    swap(nums[i], nums[j]);
+                    j--;
+                } else {
+                    j--;
+                    continue;
+                }
+            }
+            i++;
+        }
+        while(!nums.empty() && nums.back() == val) {
             nums.pop_back();
         }
         return nums;
@@ -56,12 +65,36 @@ int main() {
     //cout << ans3 << endl;
 
     vector<int> case4 = {};
-    vector<int> ans4 = mySol.removeElement(case3, 3);
+    vector<int> ans4 = mySol.removeElement(case4, 3);
     for (int i = 0; i < ans4.size(); i++) {
         cout << ans4[i] << " ";
     }
     cout << endl;
     //cout << ans4 << endl;
+
+    vector<int> case5 = {4};
+    vector<int> ans5 = mySol.removeElement(case5, 3);
+    for (int i = 0; i < ans5.size(); i++) {
+        cout << ans5[i] << " ";
+    }
+    cout << endl;
+    //cout << ans5 << endl;
+
+    vector<int> case6 = {4, 3};
+    vector<int> ans6 = mySol.removeElement(case6, 3);
+    for (int i = 0; i < ans6.size(); i++) {
+        cout << ans6[i] << " ";
+    }
+    cout << endl;
+    //cout << ans6 << endl;
+
+    vector<int> case7 = {3, 4};
+    vector<int> ans7 = mySol.removeElement(case7, 3);
+    for (int i = 0; i < ans7.size(); i++) {
+        cout << ans7[i] << " ";
+    }
+    cout << endl;
+    //cout << ans7 << endl;
 
     return 0;
 }
