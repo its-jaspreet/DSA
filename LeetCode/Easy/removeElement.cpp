c3a994239abbf9c0cc557435b3fa2b3ecf827049
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-22 15:48:34 +0530

    added to removeElement.cpp

--- LeetCode/Easy/removeElement.cpp
+++ LeetCode/Easy/removeElement.cpp
@@ -6,7 +6,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> removeElement(vector<int>& nums, int val) {
-        
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        nums.resize(j);
+        return nums;
     }
 };
 
