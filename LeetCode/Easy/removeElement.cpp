c477deefb5b8348da3d682f773a4977bbdaaf83c
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-22 15:46:10 +0530

    created removeElement.cpp

---
+++ LeetCode/Easy/removeElement.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> removeElement(vector<int>& nums, int val) {
+        
+    }
+};
+
+int main() {
+    Solution mySol;
+
+    vector<int> case1 = {3,2,2,3};
+    vector<int> ans1 = mySol.removeElement(case1, 3);
+    for (int i = 0; i < ans1.size(); i++) {
+        cout << ans1[i] << " ";
+    }
+    cout << endl;
+    //cout << ans1 << endl;
+
+    vector<int> case2 = {0,1,2,2,3,0,4,2};
+    vector<int> ans2 = mySol.removeElement(case2, 2);
+    for (int i = 0; i < ans2.size(); i++) {
+        cout << ans2[i] << " ";
+    }
+    cout << endl;
+    //cout << ans2 << endl;
+
+    return 0;
+}
