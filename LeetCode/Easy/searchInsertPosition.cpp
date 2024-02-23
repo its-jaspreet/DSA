dd102c8bba294dae2191924f928b9b6adabe76d7
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-23 22:23:42 +0530

    added to searchInsertPosition.cpp

--- LeetCode/Easy/searchInsertPosition.cpp
+++ LeetCode/Easy/searchInsertPosition.cpp
@@ -6,7 +6,34 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
+        if (nums.size() == 1) {
+            if (target > nums[nums.size() - 1]) {
+                return nums.size();
+            } else {
+                return 0;
+            }
+        }
+        int i = 0, j = nums.size() - 1;
+        while (i <= j) {
+            int mid = (i + j)/2;
+            if (target > nums[mid]) {
+                i = mid + 1;
+            } else if (target < nums[mid]) {
+                j = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        for (int k = 0; k < nums.size(); k++) {
+            if (target > nums[nums.size() - 1]) {
+                return nums.size();
+            } else {
+                if (nums[k] < target && nums[k+1] > target) {
+                    return k+1;
+                }
+            }
+        }
+        return 0;
     }
 };
 
@@ -20,7 +47,7 @@ int main() {
     int ans2 = mySol.searchInsert(case1, 2);
     cout << ans2 << endl;
 
-    int ans2 = mySol.searchInsert(case1, 7);
+    int ans3 = mySol.searchInsert(case1, 7);
     cout << ans3 << endl;
 
     return 0;
