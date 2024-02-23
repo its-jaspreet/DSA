48d30d8534447302b30377a48c66fd44a5d16c23
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-23 10:00:34 +0530

    created and added base code, and test cases to findTheIndexOfTheFirstOccurrenceInAString.cpp

---
+++ LeetCode/Easy/findTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+    }
+};
+
+int main() {
+    Solution mySol;
+
+    int ans1 = mySol.strStr("sadbutsad", "sad");
+    cout << ans1 << endl;
+
+    int ans2 = mySol.strStr("leetcode", "leeto");
+    cout << ans2 << endl;
+
+    return 0;
+}
