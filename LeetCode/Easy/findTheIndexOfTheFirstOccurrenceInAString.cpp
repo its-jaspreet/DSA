0f0e006ba981027875fca565feae74138bd9c2f3
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-23 13:56:20 +0530

    added code to findTheIndexOfTheFirstOccurrenceInAString.cpp

--- LeetCode/Easy/findTheIndexOfTheFirstOccurrenceInAString.cpp
+++ LeetCode/Easy/findTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -6,7 +6,21 @@ using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        
+        if (needle.length() > haystack.length()){
+            return -1;
+        }
+        int i = 0;
+        while (i <= haystack.length() - needle.length()) {
+            int j = 0;
+            while (j < needle.length() && haystack[i + j] == needle[j]) {
+                j++;
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
     }
 };
 
