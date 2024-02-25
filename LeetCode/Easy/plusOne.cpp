65af0c4608447a4012bc14e37be4f3d3733b5f49
its-jaspreet <itsjaspreetk@gmail.com>
2024-02-26 00:00:35 +0530

    created and added test cases

---
+++ LeetCode/Easy/plusOne.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        return digits;
+    }
+
+    void display(vector<int> &ans) {
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+int main() {
+    Solution mySol;
+
+    vector<int> case1 = {1,2,3};
+    vector<int> ans1 = mySol.plusOne(case1);
+    mySol.display(ans1);
+
+    vector<int> case2 = {4,3,2,1};
+    vector<int> ans2 = mySol.plusOne(case2);
+    mySol.display(ans2);
+
+    vector<int> case3 = {9};
+    vector<int> ans3 = mySol.plusOne(case3);
+    mySol.display(ans3);
+
+    vector<int> case4 = {};
+    vector<int> ans4 = mySol.plusOne(case4);
+    mySol.display(ans4);
+
+    vector<int> case5 = {3, 5, 9};
+    vector<int> ans5 = mySol.plusOne(case5);
+    mySol.display(ans5);
+
+    vector<int> case6 = {8, 9, 9};
+    vector<int> ans6 = mySol.plusOne(case6);
+    mySol.display(ans6);
+
+    return 0;
+}
