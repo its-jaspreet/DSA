6da958f07bd77afd496cdf65b2b4616444eb735a
its-jaspreet <itsjaspreetk@gmail.com>
2024-03-11 23:06:29 +0530

    created new file, and added base code

--- LeetCode/Medium/setMatrixZeroes.cpp
+++ LeetCode/Medium/setMatrixZeroes.cpp
@@ -6,6 +6,25 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> setZeroes(vector<vector<int>>& matrix) {
+        vector<vector<int>> temp = {};
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    temp.push_back({i, j});
+                }
+            }
+        }
+        for (int i = 0; i < temp.size(); i++) {
+            int m = temp[i][0];
+            int n = temp[i][1];
+            for (int t = 0; t < matrix[0].size(); t++) {
+                matrix[m][t] = 0;
+            }
+            for (int t = 0; t < matrix.size(); t++) {
+                matrix[t][n] = 0;
+            }
+        }
+
         return matrix;
     }
 };
