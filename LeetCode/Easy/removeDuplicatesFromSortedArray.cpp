8a770a0d02624e63168ec2c395ed3b510bc5750e
its-jaspreet <itsjaspreetk@gmail.com>
2024-03-23 12:40:04 +0530

    added code

--- LeetCode/Easy/removeDuplicatesFromSortedArray.cpp
+++ LeetCode/Easy/removeDuplicatesFromSortedArray.cpp
@@ -5,38 +5,45 @@ using namespace std;
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int prev, curr = nums.size() - 1;
-        if (curr == -1 || curr == 0) {
-            return nums.size();
-        }
-        else {
-            while (curr >= 1) {
-                prev = curr - 1;
-                if (nums[curr] == nums[prev]) {
-                    int j;
-                    for (j = curr; j < nums.size() - 1; j++) {
-                        nums[j] = nums[j+1];
-                    }
-                    nums.pop_back();
+    vector<int> removeDuplicates(vector<int> &arr) {
+        int i = arr.size() - 1;
+        int c = 1;
+        while (i >= 1) {
+            if (arr[i] == arr[i-1]) {
+                c++;
+                int j = i - 1;
+                while (arr[j] == arr[i]) {
+                    j--;
                 }
-                curr--;
+                for (int k = i; k > j; k--) {
+                    arr[k] = arr[j];
+                    j++;
+                }
+            } else {
+                c = 1;
             }
-            return nums.size();
+            i--;
         }
-    }
+        return arr;
+    }  
 };
+
+void display(vector<int>&arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }cout << endl;
+}
 
 int main() {
     Solution mySol;
 
-    vector<int> case1 = {1,1,2};
-    int ans1 = mySol.removeDuplicates(case1);
-    cout << ans1 << endl;
+    vector<int> case1 = {1,1,2,2,3,3,3};
+    vector<int> ans1 = mySol.removeDuplicates(case1);
+    display(ans1);
 
-    vector<int> case2 = {0,0,1,1,1,2,2,3,3,4};
-    int ans2 = mySol.removeDuplicates(case2);
-    cout << ans2 << endl;
+    vector<int> case2 = {0,0,1,1,1,2,2,3,3,3,4};
+    vector<int> ans2 = mySol.removeDuplicates(case2);
+    display(ans2);
 
     return 0;
 }
