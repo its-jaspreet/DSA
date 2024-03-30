96698070c37304dfa8becf53cb29f48ed76934db
its-jaspreet <itsjaspreetk@gmail.com>
2024-03-30 07:40:31 +0530

    added base code

---
+++ LeetCode/Medium/sortTheStudentsByTheirKthScore.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        
+    }
+};
+
+void display(vector<int> &arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    return 0;
+}
